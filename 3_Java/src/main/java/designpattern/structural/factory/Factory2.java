e9492c1ddc5838a694f9056bb7b95f5457e018bc
Areeb <areeb.malik1989@gmail.com>
2020-04-03 07:22:03 +0530

    Added Factory2.java

---
+++ 3_Java/src/main/java/designpattern/structural/factory/Factory2.java
@@ -0,0 +1,77 @@
+package designpattern.structural.factory;
+
+/**
+ * Product interface
+ */
+interface Shape {
+    void draw();
+}
+
+enum ShapeType {
+    LINE, CIRCLE, RECTANGLE, UNKNOWN
+}
+
+/**
+ * Concrete product
+ */
+class Line implements Shape {
+    @Override
+    public void draw(){
+        System.out.println("Drawing line.");
+    }
+}
+
+class Circle implements Shape {
+    @Override
+    public void draw(){
+        System.out.println("Drawing circle.");
+    }
+}
+
+class Rectangle implements Shape {
+    @Override
+    public void draw(){
+        System.out.println("Drawing Rectangle");
+    }
+}
+
+/**
+ * Factory class
+ */
+abstract class ShapeFactory {
+    public static Shape getShape(ShapeType type) {
+        Shape shape = null;
+        switch(type){
+            case CIRCLE:
+                return new Circle();
+            case LINE:
+                return new Line();
+            case RECTANGLE:
+                return new Rectangle();
+            default:
+                return shape;
+        }
+    }
+}
+
+class Demo {
+    
+    public static void main(String[] args) {
+        
+        // Request for circle shape
+        Shape circle = ShapeFactory.getShape(ShapeType.CIRCLE);
+        if(circle != null) {
+            circle.draw();
+        } else {
+            System.out.println("This shape cannot be drawn");
+        }
+
+        // Request non existant shape
+        Shape triangle = ShapeFactory.getShape(ShapeType.UNKNOWN);
+        if(triangle != null) {
+            triangle.draw();
+        } else {
+            System.out.println("This shape cannot be drawn");
+        }
+    }
+}
