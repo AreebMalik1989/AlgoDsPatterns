897c3e7ab1f5e043c0e1530a1e8e499b8df5b417
Areeb <areeb.malik1989@gmail.com>
2020-04-14 20:12:40 +0530

    Added condition_variable.py

---
+++ 2_Python/Concurrent/15_condition_variable.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Two hungry people, anxiously waiting for their turn to take soup """
+
+import threading
+
+slowcooker_lid = threading.Lock()
+soup_servings = 11
+soup_taken = threading.Condition(lock=slowcooker_lid)
+
+
+def hungry_person(person_id):
+    global soup_servings
+    while soup_servings > 0:
+        with slowcooker_lid:
+            while (person_id != (soup_servings % 5)) and (soup_servings > 0):
+                print('Person', person_id, 'checked... then put the lid back')
+                soup_taken.wait()
+            if soup_servings > 0:
+                soup_servings -= 1  # it's your turn; take some soup!
+                print('Person', person_id, 'took soup! Servings left:', soup_servings)
+                soup_taken.notifyAll()
+
+
+if __name__ == "__main__":
+
+    for person in range(5):
+        threading.Thread(target=hungry_person, args=(person,)).start()
