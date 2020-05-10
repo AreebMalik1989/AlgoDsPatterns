7b67f4aff5d97fc87203baf7b73cb91e98fe814c
Areeb <areeb.malik1989@gmail.com>
2020-05-10 12:33:59 +0530

    Update linear-search.js

--- 4_Javascript/algorithm/searching/linear-search.js
+++ 4_Javascript/algorithm/searching/linear-search.js
@@ -1,12 +1,26 @@
 /**
- * Linear search implementation
+ * Linear search implementation using for loop
+ * @param {T[]} array 
+ * @param {Number} key 
+ * @author Areeb
  */
-function linearSearch(array, key) {
+function linearSearch1(array, key) {
     let size = array.length;
     for(let i=0; i<size; i++) {
-        if(array[i] == key) {
-            return i;
-        }
+        if(array[i] == key) return i;
+    }
+    return null;
+}
+
+/**
+ * Linear search implementation using for of
+ * @param {T[]} array 
+ * @param {Number} key 
+ * @author Areeb
+ */
+function linearSearch2(array, key) {
+    for (const [index, value] of array.entries()) {
+        if(value === key) return index;
     }
     return null;
 }
