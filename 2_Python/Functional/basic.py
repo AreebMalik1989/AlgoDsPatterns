ca9994ab69e335a63b690aae154b6f0352f9b4fb
AreebMalik1989 <areeb.malik1989@gmail.com>
2020-11-10 17:08:13 +0530

    Update basic.py

--- 2_Python/Functional/basic.py
+++ 2_Python/Functional/basic.py
@@ -1,6 +1,6 @@
 id_ = lambda a: a
-add = lambda a: lambda b: a+b
-sub = lambda a: lambda b: a-b
-mul = lambda a: lambda b: a*b
-div = lambda a: lambda b: a//b
-mod = lambda a: lambda b: a%b
+add = lambda a: lambda b: b+a
+sub = lambda a: lambda b: b-a
+mul = lambda a: lambda b: b*a
+div = lambda a: lambda b: b//a
+mod = lambda a: lambda b: b%a
