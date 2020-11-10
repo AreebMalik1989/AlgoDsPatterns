30a2b3088d28f5107d0ee32098521666e0a9617e
AreebMalik1989 <areeb.malik1989@gmail.com>
2020-11-10 23:00:52 +0530

    Update and rename basic.py to 1_basic.py

--- 2_Python/Functional/1_basic.py
+++ 2_Python/Functional/1_basic.py
@@ -4,3 +4,10 @@ sub = lambda a: lambda b: b-a
 mul = lambda a: lambda b: b*a
 div = lambda a: lambda b: b//a
 mod = lambda a: lambda b: b%a
+
+def fizzbuzz(n: int) -> str: return (
+    'fizzbuzz' if n % 15 == 0 else
+    'fizz' if n % 3 == 0 else
+    'buzz' if n % 5 == 0 else
+    str(n)
+)
