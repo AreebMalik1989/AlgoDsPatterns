e17436e35d08035f1c7b6f1a2d9fa61b5d2e5c4b
AreebMalik1989 <areeb.malik1989@gmail.com>
2021-06-30 21:50:27 +0530

    Update binarysearch.py

--- 2_Python/Algorithm/Searching/binarysearch.py
+++ 2_Python/Algorithm/Searching/binarysearch.py
@@ -1,12 +1,12 @@
-def rbs(sa: tuple, k, s=0, e=None) -> int:
-    if e is None:
-        e = len(sa)
-    m = s + (e - s) // 2
+def recursive_binary_search(sorted_array: tuple, key, start=0, end=None) -> int:
+    if end is None:
+        end = len(sorted_array)
+    mid = start + (end - start) // 2
     return (
-        -1 if s >= e else
-        rbs(sa, k, s, m) if sa[m] > k else
-        rbs(sa, k, m+1, e) if sa[m] < k else
-        m
+        -1 if start >= end else
+        recursive_binary_search(sorted_array, key, start, mid) if sorted_array[mid] > key else
+        recursive_binary_search(sorted_array, key, mid+1, end) if sorted_array[mid] < key else
+        mid
     )
 
 
@@ -18,24 +18,14 @@ def binary_search(sorted_array, key):
     :param key: item to be found in the sorted array
     :return: index of the item in the array, or -1 if item is not found in the sorted array
     """
-
     start = 0
     end = len(sorted_array) - 1
-
     while start <= end:
-
         mid = (start + end) // 2
-
         if sorted_array[mid] > key:
-
             end = mid - 1
-
         elif sorted_array[mid] < key:
-
             start = mid + 1
-
         else:
-
             return mid
-
     return -1
