72585c76726d6bf00789d766033752d86ae5f0b1
AreebMalik1989 <areeb.malik1989@gmail.com>
2021-07-01 11:07:29 +0530

    Create linearsearch.go

---
+++ 5_Go/algorithm/search/linearsearch.go
@@ -0,0 +1,20 @@
+package linearsearch
+
+func LinearSearch1(array []int, key int) int {
+	for index, value := range array {
+		if value == key {
+			return i
+		}
+	}
+	return -1
+}
+
+func LinearSearch2(array []int, key int) int {
+	size := len(array)
+	for i := 0; i < size; i++  {
+		if array[i] == key {
+			return i
+		}
+	}
+	return -1
+}
