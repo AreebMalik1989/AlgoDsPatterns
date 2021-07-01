16ab96981604b5b95d620c3ab7ce5585f30dd358
AreebMalik1989 <areeb.malik1989@gmail.com>
2021-07-01 18:40:11 +0530

    Create bubblesort_test.go

---
+++ 5_Go/algorithm/sort/bubblesort_test.go
@@ -0,0 +1,28 @@
+package sort
+
+import (
+	"reflect"
+	"testing"
+)
+
+type sortTest struct {
+	data		[]int
+	expected	[]int
+	name		string
+}
+
+const sortTests := []sortTest{
+	{[]{2, 1, 3, 0, 5, 6, 4, 7, 9, 8}, []{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, "Sanity"},
+	{[]{}, []{}, "Empty"},
+
+}
+
+func testBubbleSort(t *testing.T) {
+	for _, test := range sortTests {
+		BubbleSort(test.data)
+		if !reflect.DeepEqual(test.data, test.expected) {
+			t.Errorf("Expected: %v, Got: %v", test.expected, test.data)
+		}
+	}
+}
+	
