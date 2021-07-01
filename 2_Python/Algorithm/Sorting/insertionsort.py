e61929adbab4588ee2668fbf32501c4fe29314b0
AreebMalik1989 <areeb.malik1989@gmail.com>
2021-07-01 21:50:06 +0530

    Update insertionsort.py

--- 2_Python/Algorithm/Sorting/insertionsort.py
+++ 2_Python/Algorithm/Sorting/insertionsort.py
@@ -5,14 +5,9 @@ def insertion_sort(array):
     :param array: unsorted array
     :return: None
     """
-
     size = len(array)
-
     for i in range(1, size):
-
         for j in range(i, 0, -1):
-
             if array[j] < array[j-1]:
-
                 array[j], array[j-1] = array[j-1], array[j]
 
