d00a7303bbee5421d56b20cb2ee456b00d99c323
AreebMalik1989 <areeb.malik1989@gmail.com>
2021-07-02 12:43:57 +0530

    Update singlylinkedlist.go

--- 5_Go/datastructure/linkedlist/singlylinkedlist.go
+++ 5_Go/datastructure/linkedlist/singlylinkedlist.go
@@ -3,13 +3,13 @@ package singlylinkedlist
 import "fmt"
 
 type node struct {
-	val	interface{}
-	next	*node
+	Val	interface{}
+	Next	*node
 }
 
 type SinglyLinkedList struct {
 	length	int
-	head	*node
+	Head	*node
 }
 
 func CreateList() *SinglyLinkedList {
@@ -26,65 +26,65 @@ func (ll *SinglyLinkedList) Count() int {
 
 func (ll *SinglyLinkedList) AddAtBeg(val interface{}) {
 	n := NewNode(val)
-	n.next = ll.head
-	ll.head = n
+	n.Next = ll.Head
+	ll.Head = n
 	ll.length++
 }
 
 func (ll *SinglyLinkedList) AddAtEnd(val interface{}) {
 	n := NewNode(val)
-	if ll.head == nil {
-		ll.head = n
+	if ll.Head == nil {
+		ll.Head = n
 		ll.length++
 		return
 	}
-	cur := ll.head
-	for ; cur.next != nil; cur = cur.next {
+	cur := ll.Head
+	for ; cur.Next != nil; cur = cur.Next {
 	}
-	cur.next = n
+	cur.Next = n
 	ll.length++
 }
 
 func (ll *SinglyLinkedList) DelAtBeg() interface{} {
-	if ll.head == nil {
+	if ll.Head == nil {
 		return -1
 	}
-	cur := ll.head
-	ll.head = cur.next
+	cur := ll.Head
+	ll.Head = cur.Next
 	ll.length--
-	return cur.val
+	return cur.Val
 }
 
 func (ll *SinglyLinkedList) DelAtEnd() interface{} {
-	if ll.head == nil {
+	if ll.Head == nil {
 		return -1
-	} else if ll.head.next = nil {
+	} else if ll.Head.Next = nil {
 		return DelAtBeg()
 	}
-	cur := ll.head
-	for ; cur.next.next != nil; cur = cur.next {
+	cur := ll.Head
+	for ; cur.next.Next != nil; cur = cur.Next {
 	}
-	val := cur.next.val
-	cur.next = nil
+	val := cur.Next.Val
+	cur.Next = nil
 	ll.length--
 	return val
 }
 
 func (ll *SinglyLinkedList) Reverse() {
 	var prev, next *node
-	cur = ll.head
+	cur = ll.Head
 	for cur != nil {
-		next = cur.next
-		cur.next = prev
+		next = cur.Next
+		cur.Next = prev
 		prev = cur
 		cur = next
 	}
-	ll.head = prev
+	ll.Head = prev
 }
 
 func (ll *SinglyLinkedList) Display() {
-	for cur := ll.head; cur != nil; cur = cur.next {
-		fmt.Print(cur.val, " ")
+	for cur := ll.Head; cur != nil; cur = cur.Next {
+		fmt.Print(cur.Val, " ")
 	}
 	fmt.Print("\n")
 }
