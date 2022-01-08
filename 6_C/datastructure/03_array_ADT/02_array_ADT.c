a7813da3b2a83cd9b11c042158f151b19b9ab3be
AreebMalik1989 <areeb.malik1989@gmail.com>
2022-01-09 01:38:38 +0530

    Update 02_array_ADT.c

--- 6_C/datastructure/03_array_ADT/02_array_ADT.c
+++ 6_C/datastructure/03_array_ADT/02_array_ADT.c
@@ -105,3 +105,54 @@ int RBinarySearch(struct Array arr, int key, int l, int h)
     }
     return -1;
 }
+
+int Get(struct Array arr, int index)
+{
+    if(index >=0 && index < arr.length)
+        return arr.A[index];
+    return -1;
+}
+
+void Set(struct Array *arr, int index, int x)
+{
+    if(index >=0 && index < arr->length)
+        arr->A[index] = x;
+}
+
+int Max(struct Array arr)
+{
+    int max = arr.A[0];
+    int i;
+    for(i=0; i<arr.length; i++)
+    {
+        if(arr.A[i] > max)
+            max = arr.A[i];
+    }
+    return max;
+}
+
+int Min(struct Array arr)
+{
+    int min = arr.A[0];
+    int i;
+    for(i=0; i<arr.length; i++)
+    {
+        if(arr.A[i] < min)
+            min = arr.A[i];
+    }
+    return min;
+}
+
+int Sum(struct Array arr)
+{
+    int s = 0;
+    int i;
+    for(i=0; i<arr.length; i++)
+        s += arr.A[i];
+    return s;
+}
+
+float Avg(struct Array arr)
+{
+    return (float)Sum(arr)/arr.length;
+}
