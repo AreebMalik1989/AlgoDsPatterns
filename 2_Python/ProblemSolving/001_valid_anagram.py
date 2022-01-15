7109260f83c34b29cfa9c7be0321310432fd7786
AreebMalik1989 <areeb.malik1989@gmail.com>
2022-01-15 17:22:30 +0530

    Create 001_valid_anagram.py

---
+++ 2_Python/ProblemSolving/001_valid_anagram.py
@@ -0,0 +1,34 @@
+from collections import Counter
+
+
+def are_anagrams1(s1: str, s2: str):
+    if len(s1) != len(s2):
+        return False
+    freq1 = dict()
+    freq2 = dict()
+    for ch in s1:
+        if ch in freq1:
+            freq1[ch] += 1
+        else:
+            freq1[ch] = 1
+    for ch in s2:
+        if ch in freq2:
+            freq2[ch] += 1
+        else:
+            freq2[ch] = 1
+    for key in freq1:
+        if key not in freq2 or freq1[key] != freq2:
+            return False
+    return True
+
+
+def are_anagrams2(s1: str, s2: str):
+    if len(s1) != len(s2):
+        return False
+    return Counter(s1) == Counter(s2)
+
+
+def are_anagrams3(s1: str, s2: str):
+    if len(s1) != len(s2):
+        return False
+    return sorted(s1) != sorted(s2)
