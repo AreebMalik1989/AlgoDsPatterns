c4f8056569d08c4c2ea1de0ef8aac5228b82c4fb
AreebMalik1989 <areeb.malik1989@gmail.com>
2022-01-23 07:32:03 +0530

    Update 02_array_ADT.c

--- 6_C/datastructure/03_array_ADT/02_array_ADT.c
+++ 6_C/datastructure/03_array_ADT/02_array_ADT.c
@@ -220,3 +220,29 @@ void Insert(struct Array *sortedArray, int x)
     sortedArray->length++;
 }
 
+// Merge sorted arrays
+struct Array* Merge(struct Array *arr1, struct Array *arr2)
+{
+    int i, j, k;
+    i=j=k=0;
+    
+    struct Array *arr3 = (struct Array *)malloc(sizeof(struct Array));
+    
+    while(i < arr1->length && j < arr2->length)
+    {
+        if(arr1->A[i] < arr2->A[j])
+            arr3->A[k++] = arr1->A[i++];
+        else
+            arr3->A[k++] = arr2->A[j++];
+    }
+    
+    while(i < arr1->length)
+        arr3[k++] = arr1->A[i++];
+    while(j < arr2->length)
+        arr3[k++] = arr1->A[j++];
+    
+    arr3->length = arr1->length + arr2->length;
+    arr3->size = 10;
+    
+    return arr3;
+}
