2b9217633a30f1749d945c021c756d0af6bdf8a3
AreebMalik1989 <areeb.malik1989@gmail.com>
2022-02-20 15:24:42 +0530

    Update 1_basic.js

--- 4_Javascript/functional/1_basic.js
+++ 4_Javascript/functional/1_basic.js
@@ -1,4 +1,9 @@
+// Unary
 const id  = x => x;
+const inc = x => x + 1;
+const dec = x => x - 1;
+
+// Binary
 const add = a => b => b + a;
 const sub = a => b => b - a;
 const mul = a => b => b * a;
