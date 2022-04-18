e9598b9f84372ffdf54594a88e47760b25a181b0
AreebMalik1989 <areeb.malik1989@gmail.com>
2022-04-18 15:50:21 +0530

    Create 01A_ThreadLocal.java

---
+++ 3_Java/src/main/java/concurrent/java_concurrency/01A_ThreadLocal.java
@@ -0,0 +1,39 @@
+public class UserService {
+
+	private static final ExecutorService threadPool = Executors.newFixedThreadPool(10);
+
+	public static void main(String[] args) {
+		final THREAD_COUNT = 1000;
+		for (int i=0; i<THREAD_COUNT; i++) {
+			int id = i;
+			threadPool.submit(() -> {
+				String birthDate = new UserService().birthDate(id);
+				System.out.println(birthDate);
+			});
+		}
+		Thread.sleep(1000);
+	}
+
+	public String birthDate(int userId) {
+		Date birthDate = birthDateFromDB(userId);
+		final SimpleDateFormat df = ThreadSafeFormatter.dateFormatter.get();
+		return df.format(birthDate);
+	}
+	
+}
+
+class ThreadSafeFormatter {
+
+	public static ThreadLocal<SimpleDateFormat> dateFormatter = new ThreadLocal<SimpleDateFormatter>() {
+
+		@Override
+		protected SimpleDateFormat initialValue() {
+			return new SimpleDateFormat("yyyy-MM-dd");
+		}
+
+		@Override
+		public SimpleDateFormat get() {
+			return super.get();
+		}
+	}
+}
