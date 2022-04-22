7562619df03f4349bfceeef08edcec529056099a
AreebMalik1989 <areeb.malik1989@gmail.com>
2022-04-22 16:50:50 +0530

    Update 03C_ExecutorService.java

--- 3_Java/src/main/java/concurrent/java_concurrency/03C_ExecutorService.java
+++ 3_Java/src/main/java/concurrent/java_concurrency/03C_ExecutorService.java
@@ -1,10 +1,11 @@
 public class JavaScheduledThreadPool {
     
-    static Runnable task = () -> System.out.println("Thread Name: " + Thread.currentThread().getName());
+    private static Runnable task = () -> System.out.println("Thread Name: " + Thread.currentThread().getName());
+    private static boolean shouldCancel = true;
 
     public static void main(String[] args) {
-        
-        ScheduledExecutorService service = Executors.newScheduledThreadPool(Runtime.getRuntime().availableProcessors());
+
+        ScheduledExecutorService service = Executors.newWorkStealingPool()
 
         final int INITIAL_DELAY = 10;
         final int DELAY = 12;
@@ -17,8 +18,13 @@ public class JavaScheduledThreadPool {
         service.scheduleAtFixedRate(task, INITIAL_DELAY, PERIOD, TimeUnit.SECONDS);
 
         // task run repeatedly DELAY seconds after previous task completes
-        service.scheduleWithFixedDelay(task, INITIAL_DELAY, DELAY, TimeUnit.SECONDS);
+        ScheduledFuture<?> scheduledFuture = service.scheduleWithFixedDelay(task, INITIAL_DELAY, DELAY, TimeUnit.SECONDS);
         
+        if(shouldCancel) {
+            boolean shouldInterrupt = false;
+            scheduledFuture.cancel(shouldInterrupt);
+        }
+
         service.shutdown();
     }
 }
