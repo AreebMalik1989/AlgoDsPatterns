a5190655f67a9a6974f52fef4e8ada3ba26980e6
AreebMalik1989 <areeb.malik1989@gmail.com>
2022-11-01 23:04:41 +0530

    Create 04A_ConditionClass.java

---
+++ 3_Java/src/main/java/concurrent/java_concurrency/04A_ConditionClass.java
@@ -0,0 +1,38 @@
+public class SharedStack<T> {
+
+    private final int capacity;
+    private Stack<T> stack = new Stack<>();
+    private ReentrantLock lock = new ReentrantLock();
+    private Condition stackEmptyCondition = lock.newCondition();
+    private Condition stackFullCondition = lock.newCondition();
+
+    public SharedStack(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void push(T item) throws InterruptedException {
+        try {
+            lock.lock();
+            while (stack.size() == capacity) { // Need to use 'while' instead of 'if' to guard against spurious wake up
+                stackFullCondition.await();
+            }
+            stack.push(item);
+            stackEmptyCondition.notifyAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public T pop() throws InterruptedException {
+        try {
+            lock.lock();
+            while (stack.size() == 0) { // Need to use 'while' instead of 'if' to guard against spurious wake up
+                stackEmptyCondition.await();
+            }
+            return stack.pop();
+        } finally {
+            stackFullCondition.signalAll();
+            lock.unlock();
+        }
+    }
+}
