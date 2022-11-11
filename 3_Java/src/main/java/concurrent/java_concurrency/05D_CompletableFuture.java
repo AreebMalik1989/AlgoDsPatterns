8f55a318b2a37323749069660440fa16ec28c1c2
AreebMalik1989 <areeb.malik1989@gmail.com>
2022-11-11 23:31:53 +0530

    Update 05D_CompletableFuture.java

--- 3_Java/src/main/java/concurrent/java_concurrency/05D_CompletableFuture.java
+++ 3_Java/src/main/java/concurrent/java_concurrency/05D_CompletableFuture.java
@@ -4,8 +4,8 @@ public class IOBoundExample {
         ExecutorService ioBound = Executors.newCachedThreadPool();
         for(int i=0; i<100; i++) {
             CompletableFuture.supplyAsync(() -> getOrder(), ioBound)    // performed in ioBound thread pool
-                    .thenApply(order -> enrich(order), cpuBound)    // performed in cpuBound thread pool
-                    .thenApply(order -> performPayment(order), ioBound) // performed in ioBound thread pool
+                    .thenApplyAsync(order -> enrich(order), cpuBound)    // performed in cpuBound thread pool
+                    .thenApplyAsync(order -> performPayment(order), ioBound) // performed in ioBound thread pool
                     .exceptionally(e -> new FailedOrder())  // catch exception in any of above steps
                     .thenApply(order -> dispatch(order))    // performed in default fork join pool
                     .thenAccept(order -> sendEmail(order)); // performed in default fork join pool
